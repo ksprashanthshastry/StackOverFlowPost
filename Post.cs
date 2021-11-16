202b93af9417626f500f0f796d539c325f91aa72
ksprashanthshastry <ksprashanthshastry@gmail.com>
2021-11-16 12:53:34 +1000

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace StackOverFlowPost
+{
+    public class Post
+    {
+        private string _title;
+        private string _description;
+        private DateTime _dateCreated;
+        private int _vote = 0;
+
+        public string Title 
+        {
+            get { return this._title; } 
+            set { this._title = value; } 
+        }
+
+        public string Description
+        {
+            get { return this._description; }
+            set {this._description = value; } 
+        }
+
+        public DateTime DateCreated
+        {
+            get { return this._dateCreated; }
+        }
+
+        public Post(string title, string description)
+        {
+            this._title = title;
+            this._description = description;
+            this._dateCreated = DateTime.Now;
+        }
+
+        public Post(string title)
+        {
+            this._title = title;
+            this._dateCreated = DateTime.Now;
+        }
+
+        public Post()
+        {
+            this._dateCreated = DateTime.Now;
+        }
+
+        public int VoteCount 
+        {
+            get { return this._vote; }  
+        }
+
+        public void UpVote()
+        {
+            _vote ++;
+        }
+
+        public void DownVote()
+        {
+            _vote--;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StackOverFlowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var post = new Post("first post", "this is the first post");
+
+            post.UpVote();
+            Console.WriteLine(post.VoteCount);
+
+            var post1 = new Post();
+            post1.Title = "second post";
+            post1.Description = "this is the 2nd post";
+            post1.UpVote();
+            post1.UpVote();
+            post1.UpVote();
+            post1.UpVote();
+            post1.DownVote();
+            post1.DownVote();
+
+            Console.WriteLine("2nd Post total votes: " + post1.VoteCount + " Post was created on " + post1.DateCreated);
+        }
+    }
+}
